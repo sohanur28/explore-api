da338d7e02d4cf19eda76f892c8cae6af1ccb4fd
sohanur28 <gsrahman28@gmail.com>
2025-07-02 10:44:14 +0600

    debug api

--- js/post.js
+++ js/post.js
@@ -1,7 +1,7 @@
-function loadPost(){
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(data => displayPosts(data))   
+function loadPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => displayPosts(data));
 }
 
 /*
@@ -11,19 +11,19 @@ function loadPost(){
 4, appendChild
  */
 
-function displayPosts(posts){
-    const postContainer = document.getElementById('post-container');
-    for( const post of posts){
-        console.log(post);
-        
-        const postDiv = document.createElement('div');
-        postDiv.classList.add('post')
-        postDiv.innerHTML = `
+function displayPosts(posts) {
+  const postContainer = document.getElementById("post-container");
+  for (const post of posts) {
+    console.log(post);
+
+    const postDiv = document.createElement("div");
+    postDiv.classList.add("post");
+    postDiv.innerHTML = `
         <h4>User-${post.userId}</h4>
         <h5>Post: ${post.title}</h5>
         <p>Post Description: ${post.body}</p>
         `;
-        postContainer.appendChild(postDiv);
-    }
+    postContainer.appendChild(postDiv);
+  }
 }
 loadPost();
